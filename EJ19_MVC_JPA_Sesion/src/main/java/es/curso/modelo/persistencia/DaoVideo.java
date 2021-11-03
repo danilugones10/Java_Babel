b66f5def2ee1ebf67931ee51a7f5d48355bdb43c
Daniel Cabal Suarez <danilugones10@gmail.com>
2021-11-03 12:47:29 +0100

    EJ19 borrando

--- EJ19_MVC_JPA_Sesion/src/main/java/es/curso/modelo/persistencia/DaoVideo.java
+++
@@ -1,9 +0,0 @@
-package es.curso.modelo.persistencia;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.curso.modelo.entidad.Videojuego;
-
-public interface DaoVideo extends JpaRepository<Videojuego, Integer> {
-
-}
